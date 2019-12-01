1046a7ab058e99e8a4268467e49de385abf38b53
kalvyn <19240589@sun.ac.za>
2019-12-01 08:14:04 +0200

    Update api.py

--- api.py
+++ api.py
@@ -20,4 +20,4 @@ class HelloWorld(Resource):
 api.add_resource(HelloWorld, '/', '/<string:s1>', '/<string:s1>/<string:s2>')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0')
